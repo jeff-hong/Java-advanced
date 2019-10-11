43b9c866f0fb3cf64c13040d98c95cc68ef18593
Ahuan <535948509@qq.com>
2019-10-11 20:55:53 +0800

    第一次提交

---
+++ day01/src/com/atguigu/threadcase/SaleTicket.java
@@ -0,0 +1,90 @@
+package com.atguigu.threadcase;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Ticket {
+    /*private int ticket = 30;
+    public synchronized void sale(){
+        if(ticket > 0) {
+            System.out.println(Thread.currentThread().getName() + "\t卖出第" + ticket-- + "张\t 还剩下: " + ticket + "张");
+        }
+    }*/
+
+    private int ticket = 30;
+    Lock lock = new ReentrantLock();
+    //lock取代synchronized
+    public void sale() {
+        lock.lock();
+        try {
+            if(ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + "\t卖出第" + ticket-- + "张\t 还剩下: " + ticket + "张");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 题目：三个售票员     卖出      30张票
+ * 目的：如何写出企业级的多线程程序
+ * <p>
+ * ** 高内聚，低耦合
+ * <p>
+ * 1 高内低耦的前提下，线程       操作          资源类
+ */
+public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+
+        new Thread(() -> {
+            for (int i = 0; i < 29; i++) {
+                ticket.sale();
+            }
+        }, "C").start();
+        new Thread(() -> {
+            for (int i = 0; i < 29; i++) {
+                ticket.sale();
+            }
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 29; i++) {
+                ticket.sale();
+            }
+        }, "A").start();
+
+
+        //Thread(Runnable target, String name) Allocates a new Thread object.
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 39; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "A").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 39; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "B").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 39; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "C").start();*/
+    }
+}
